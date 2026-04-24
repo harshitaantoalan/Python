865ac4162e360a3d00d134c2ca27a150ede1d0df
harshitaantoalan <harshitaantoalan@gmail.com>
2026-04-24 16:57:28 +0530

    Add files via upload

---
+++ numberswap.py
@@ -0,0 +1,8 @@
+# Take input values from user
+x = input("Enter Value of x:")
+y = input("Enter Value of y:")
+temp = x
+x = y
+y = temp
+print("value of x after swapping", x)
+print("value of y after swapping", y)
