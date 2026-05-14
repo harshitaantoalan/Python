7e1a2b960c7b2083923831b3ac2a07105dbb91d1
harshitaantoalan <harshitaantoalan@gmail.com>
2026-05-14 19:49:08 +0530

    Add files via upload

---
+++ OOP2.py
@@ -0,0 +1,9 @@
+class Employee:
+    #Intializing (Constructor)
+    def __init__(self):
+        print('Employee created')
+    #Deleting(Destructor)
+    def __del__(self):
+        print('Destructor called,Employee deleted')
+obj=Employee()
+del obj
