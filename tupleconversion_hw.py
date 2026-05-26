929cc5aff5b94f225b1d796e3993a034e44bc0c5
harshitaantoalan <harshitaantoalan@gmail.com>
2026-05-26 19:50:45 +0530

    Add files via upload

---
+++ tupleconversion_hw.py
@@ -0,0 +1,9 @@
+# Storing personal details in a tuple
+details = ("Harshita", "Alan", 16, 160, 50, "Math")
+
+# Converting tuple into list
+details_list = list(details)
+
+# Printing the list
+print("Tuple converted into list:")
+print(details_list)
