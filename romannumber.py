d0cdaec909ea4e430423f2efa80db8db95a931ac
harshitaantoalan <harshitaantoalan@gmail.com>
2026-07-28 16:50:54 +0530

    Add files via upload

---
+++ romannumber.py
@@ -0,0 +1,13 @@
+def romantoInt(romanInput):
+    roman={'M':1000,'D':500,'C':100,'L':50,'V':5,'I':1}
+    resultInteger=0
+    for i in range (0,len(romanInput)-1):
+        if roman[romanInput[i]]< roman[romanInput[i+1]]:
+            resultInteger-=roman[romanInput[i]]
+        else:
+            resultInteger+=roman[romanInput[i]]
+    return resultInteger+roman[romanInput[-1]]
+roman = input("Input roman numeral")
+print("Integer equivalent:",romantoInt(roman))
+
+                 
